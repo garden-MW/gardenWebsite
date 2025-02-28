48cc1c863b8b7399f93755b76cd553d965fa2f5b
Oliscia Thornton <okthornton@middlebury.edu>
2025-02-28 15:23:20 -0500

    bar graph component

--- src/app/overview/page.js
+++ src/app/overview/page.js
@@ -2,6 +2,13 @@ import GraphDash from "@/components/overviewGraph"
 
 export default function Overview(){
     return(
-        <GraphDash />
+        <div className="flex justify-center h-screen w-screen">
+            <div className="w-full h-full max-w-[90%] max-h-[30%] flex justify-center items-center">
+                <GraphDash />
+            
+            </div>
+        </div>
+       
+        
     )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,6 @@ export default function Home() {
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="./overview"
-            target="_blank"
             rel="noopener noreferrer"
           >
             Overview Page
